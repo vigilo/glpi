8d255750813168d5adcab8216d0f8f12f79ebe42
Francois POIROTTE <francois.poirotte@c-s.fr>
2017-05-24 19:31:41 +0200

    Récupération des services en fonction du test
    Permet d'ajouter automatiquement une dépendance sur les services
    générés par les tests que le plugin a configuré dans le HLS
    de l'hôte.
    
    Change-Id: I25986aa113bdf2ac589720fb53c838c301cf7096
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/2400
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>

--- src/plugins/vigilo/Vigilo/VigiloTest.php
+++ src/plugins/vigilo/Vigilo/VigiloTest.php
@@ -18,6 +18,71 @@ class VigiloTest extends VigiloXml implements ArrayAccess
     public function getNagiosNames()
     {
         $res = array();
+        switch ($this->name) {
+            case 'Interface':
+                // Pour une interface réseau, le service est construit
+                // à partir du label donné.
+                $label = $this->args['label']->getValue();
+                $res[] = "Interface $label";
+
+                // On regarde si des seuils ont été définis ou non.
+                // Si c'est le cas, cela génère des services supplémentaires.
+                $warn   = isset($this->args['warn']) ? $this->args['warn'] : array();
+                $crit   = isset($this->args['crit']) ? $this->args['crit'] : array();
+                $tests  = array(
+                    "Traffic in",
+                    "Traffic out",
+                    "Discards in",
+                    "Discards out",
+                    "Errors in",
+                    "Errors out",
+                );
+                if (is_array($warn) && is_array($crit)) {
+                    foreach ($tests as $i => $test) {
+                        if (isset($warn[$i], $crit[$i])) {
+                            $res[] = "$test $label";
+                        }
+                    }
+                }
+                break;
+
+            case 'Partition':
+                // Le label est utilisé pour construire le nom du service.
+                $label = $this->args['label']->getValue();
+                $res[] = "Partition $label";
+                break;
+
+            case 'Ping':
+                $res[] = 'Ping';
+                break;
+
+            case 'Process':
+                $label = $this['label'];
+                if (null === $label) {
+                    $label = $this->args['processname'];
+                }
+                $label = $label->getValue();
+                $res[] = "Process $label";
+                break;
+
+            case 'Service':
+                $label = $this['label'];
+                if (null === $label) {
+                    $label = $this->args['svcname'];
+                }
+                $res[] = $label->getValue();
+                break;
+
+            case 'TCP':
+                $label = $this['label'];
+                if (null === $label) {
+                    $label = 'TCP ' . $this->args['port']->getValue();
+                } else {
+                    $label = $label->getValue();
+                }
+                $res[] = $label;
+                break;
+        }
 
         return $res;
     }
